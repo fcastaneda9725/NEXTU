2f55643d94b229e4ece5b0324afa138dde425d06
fcastaneda9725 <f.castanedat@gmail.com>
2018-03-26 11:55:00 -0600

    Ejercicios JS

---
+++ Ejercicios JS/Ejercicio 2/js/index.js
@@ -0,0 +1,20 @@
+
+document.getElementById('boton-receta1').addEventListener('click', function(){
+var ingrediente1 = "Claras de huevo", cantidad1 = 5, ingrediente2 = "Azucar Blanca", cantidad2 = 150, ingrediente3 ="Leche Condensada", cantidad3 = 200, ingrediente4 = "Gelatina sin Sabor", cantidad4 = 1, ingrediente5 = "Limones", cantidad5 = 4;
+console.log("Preparando Poste de limón");
+console.log("Primero debes adicionar las " + cantidad1 +" "+ ingrediente1 +" en un Bol y batir con los "+ cantidad2 + " gramos de " + ingrediente2);
+console.log("; luego debes adicionar los " + cantidad3 + " gramos de " + ingrediente3 +" y batir bien. Una vez termines con esto, adiciona el zumo de los ");
+console.log(cantidad5 + " " + ingrediente5 + " y la "+ingrediente4+" hidratada en agua tibia. Por último deposita la mezcla en moldes individaules y mételo a la nevera. ¡En 30 minutos to postre estará listo!");
+})
+
+document.getElementById('boton-receta2').addEventListener('click', function(){
+var ingrediente1 = "Ginebra", cantidad1 = 2.5, ingrediente2 = "Vermounth Seco", cantidad2 = 0.5, ingrediente3 = "Aceituna", cantidad3 = 1;
+console.log("Preparando Martini");
+console.log("Para preparar un Martini básico debes verter las "+cantidad1+" onzas de "+ingrediente1+" en una coctelera y anadir ");
+console.log("las "+cantidad2+" onzas de "+ingrediente2+". Para servir, pon la mezcla en una copa tipo Martini y añade "+cantidad3+" "+ingrediente3);
+
+
+
+
+
+})
