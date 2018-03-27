defd19f90f1acd89c7066d2fcce80c1c69d78c2e
fcastaneda9725 <f.castanedat@gmail.com>
2018-03-27 10:16:57 -0600

    Ajustes de Ejercicio

--- Ejercicios JS/Ejercicio 3/js/index.js
+++ Ejercicios JS/Ejercicio 3/js/index.js
@@ -1,4 +1,5 @@
 
-document.getElementById('boton-perfil').addEventListener("click", function(){
-  
-})
+document.getElementById('boton-perfil').addEventListener('click', function () {
+  var nombre = 'Gabriel Santos', ciudad = 'Nueva York', entidad = 'Salud Para ti';
+  console.log('Nombre: ' + nombre + ciudad + entidad);
+});
