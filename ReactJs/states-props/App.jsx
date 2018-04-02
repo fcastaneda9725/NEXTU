d9167e19b90ee4f2ce39bbbf2cf17ddf81a2db0d
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-02 00:48:29 -0500

    No me salio

--- ReactJs/states-props/App.jsx
+++ ReactJs/states-props/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import Componente2 from './componente2.jsx';
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = { mensaje: 'Bienbenido a tu peor pesadilla con React Js'}
+    this.state = { mensaje: 'Bienvenido a tu peor pesadilla con React Js',
+                    mensaje2: 'Segundo mensaje con Props'
+          }
   }
 
   render() {
-    return {
+    return(
       <div>
         <h1>{ this.state.mensaje } </h1>
+        <Componente2 mensajeProps={this.state.mensaje2}/>
+        <div>
+          <button onClick= {this.changeState.bind(this)}> Cambiar mensaje </button>
+        </div>
       </div>
-    }
+    )
+  }
+
+  changeState() {
+    this.setState( { mensaje: 'Nuevo mensaje desde el infierno'})
   }
 }
 
---
+++ ReactJs/states-props/componente2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Componente2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{his.props.mensajeProps}</h2>
+      </div>
+    )
+  }
+}
+
+export default Componente2;
