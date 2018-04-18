d387b6a910832ecfffa6ac5b5a45b639e75d5ebe
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-18 13:15:50 -0500

    Ultima prueba firebase, instalación de cordova apache

---
+++ Angular/firebase/src/app/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
+import { HttpService } from './http.service';
+import { Response } from '@angular/http';
+
+@Injectable()
+export class DataService {
+  private usuarios: string[] = [];
+
+  constructor(private logService: LogService, private httpService : HttpService){ }
+
+ 
+
+  newUser(nombre: string){
+    this.usuarios.push(nombre);
+    this.logService.escribirLog('Se añadio el usuario '+ nombre);
+  }
+
+  getUsers(){
+    this.httpService.getDatos()
+      .subscribe(
+        (data: Response) => console.log(data)
+      )
+    return this.usuarios;
+  }
+  
+
+}
