ac0bb8e545cdaaa03441f471df466f5f5eb906f3
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-23 11:00:51 -0500

    React-Native

---
+++ ReactNative/diccionario/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import Titulo from './src/component/titulo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Titulo/>
+    );
+  }
+}
