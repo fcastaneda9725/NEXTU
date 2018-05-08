455f1a20e0b64552cd762ae66060bc847430584b
fcastaneda9725 <f.castanedat@gmail.com>
2018-05-08 12:28:03 -0500

    Config Firebase

--- ReactNative/login/src/componentes/App.js
+++ ReactNative/login/src/componentes/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Encabezado, Boton } from './lib';
 
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyBqdCa7ny1R7jrwiaTTvLA-U1tjp34WkK4',
+        authDomain: 'login-2517e.firebaseapp.com',
+        databaseURL: 'https://login-2517e.firebaseio.com',
+        projectId: 'login-2517e',
+        storageBucket: 'login-2517e.appspot.com',
+        messagingSenderId: '683465112972',
+      });
+  }
+
   render() {
     return (
       <View>
