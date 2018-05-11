b1cf692c70fe93bdc07ed01281304f73ce62a59f
fcastaneda9725 <f.castanedat@gmail.com>
2018-05-11 15:08:23 -0500

    Redux y configuración

---
+++ ReactNative/reduxLista/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={ createStore(reducers) }>
+
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
